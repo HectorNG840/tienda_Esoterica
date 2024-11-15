7e7f45a3800ca56284d9f31e38857a246d8cf6df
HectorNG840 <hecnoggon@alum.us.es>
2024-11-15 12:14:18 +0100

    Feat: Adding Products and home screen

--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from tiendaEsoterica import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Página principal
 ]
+
+
