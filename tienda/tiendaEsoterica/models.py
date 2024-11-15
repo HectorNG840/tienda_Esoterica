a6928738229ee755998389af0349c463ed84655a
Porti45 <alonsoportillo.sanchez@gmail.com>
2024-11-15 12:07:08 +0100

    feat: Posible creación en backend de productos. Está comentado

--- tienda/tiendaEsoterica/models.py
+++ tienda/tiendaEsoterica/models.py
@@ -7,3 +7,12 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+# class Producto(models.Model):
+#     nombre = models.CharField(max_length=100)
+#     descripcion = models.TextField()
+#     precio = models.DecimalField(max_digits=10, decimal_places=2)
+#     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+
+#     def __str__(self):
+#         return self.nombre
