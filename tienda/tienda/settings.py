42669137ece29fcc73505a669ba16d437d896ef2
sheche1 <92683001+sheche1@users.noreply.github.com>
2024-11-16 16:39:53 +0100

    fix:corregir errores

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -126,4 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/perfil/'  # Cambia '/perfil/' por la URL de destino
+
 
