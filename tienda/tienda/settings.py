022557f3267ffb399a9b06b3e1af2bc6736ba3ba
Ruben Perez <rupg29@gmail.com>
2024-11-18 20:03:39 +0100

    fix: backend authentication restrictions

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tienda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Esta línea debe apuntar a la carpeta `templates`
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,6 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 8},
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -135,14 +136,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# En settings.py
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'  # Almacena sesiones en cache
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Cierra sesión al cerrar el navegador
 
 LOGIN_REDIRECT_URL = '/inicio/'  # Redirige a la página de inicio después de iniciar sesión
 LOGOUT_REDIRECT_URL = '/login/'  # Redirige a la página de login después de cerrar sesión
 
-
-
-
-
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
