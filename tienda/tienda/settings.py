215b82b01f48bc9a639ef95d5cd594221883c236
HectorNG840 <hecnoggon@alum.us.es>
2024-11-19 22:24:15 +0100

    Fix: Allowed hosts

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-oky8)3#_3i79-=4b#-ay2%438u)790d!n-r!*sq_b_f_3ln=&x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tienda-esoterica.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
