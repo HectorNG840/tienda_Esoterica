2d4e3ec619a8a85bbf5999940f6bf9fd1baaa01d
Porti45 <alonsoportillo.sanchez@gmail.com>
2024-12-04 09:06:40 +0100

    Nuevo correo

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -158,5 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hectorng840@gmail.com'
-EMAIL_HOST_PASSWORD = 'sffgyeapgnqdfsus'
+EMAIL_HOST_USER = 'tiendaesotericamisterio@gmail.com'
+EMAIL_HOST_PASSWORD = 'jbzdbjlwtqjojqmc'
+#tiendaesotericamisterio@gmail.com
+#Tienda1234
